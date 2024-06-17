9f723c0d392406b1a58dd7e909e42c119eb75f15
Simon <kh931623@hotmail.com>
2024-06-18 03:12:21 +0800

    feat(ui-lib): button children

--- ui-lib/lib/components/Button/index.jsx
+++ ui-lib/lib/components/Button/index.jsx
@@ -1,9 +1,11 @@
 import { Button } from '@headlessui/react'
 
-function BaseButton() {
+function BaseButton({
+    children
+}) {
     return (
         <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
-            Save changes
+            {children}
         </Button>
     )
 }

39165b5a8917c109362f487f4663201e1247ed7e
Simon <kh931623@hotmail.com>
2024-06-18 03:12:03 +0800

    feat(github-service): get repo method

--- github-service/src/index.js
+++ github-service/src/index.js
@@ -2,13 +2,28 @@ import { Octokit } from "octokit";
 
 class GithubService {
     constructor(token) {
+        this.token = token
         this.client = new Octokit({
             auth: token
         })
     }
 
     test() {
-        console.log('hello');
+        console.log(this.token);
+    }
+
+    async getRepos(keyword) {
+        try {
+            const { data } = await this.client.request('GET /search/repositories', {
+                q: keyword,
+                per_page: 10,
+            })
+
+            return data
+        } catch (error) {
+            console.error(error);
+            return []
+        }
     }
 }
 
